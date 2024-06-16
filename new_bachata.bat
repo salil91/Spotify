18a8a86c7bc6f06962076581d5f0f2d11664ea6f
Salil Bavdekar <salil91@gmail.com>
2024-06-15 22:10:41 -0400

    comments

--- new_bachata.bat
+++ new_bachata.bat
@@ -1,26 +1,25 @@
 @echo OFF
-rem This example searches for "bachata" tracks released in the last 10 days
 
-rem Define the genre and the number of days
-set GENRE=bachata
-set DAYS=8
+:: Define the parameters for the search
 set ARTISTS_LIST="Bachata Artists (Manual).csv"
+set GENRE=bachata  &:: only used to name the playlist, since artist list is given
+set DAYS=8
 
-rem Define the path to the yaml file with the spotify API parameters
+:: Define the path to the yaml file with the spotify API parameters
 set SPOTIFY_YAML=spotify.yaml
 
-rem Define the path to your conda installation
+:: Define the path to your conda installation
 set MAMBAPATH=%USERPROFILE%\miniforge3
-rem Define the name of the environment
+:: Define the name of the environment
 set ENVNAME=spotify
 
-rem Activate the conda environment
+:: Activate the conda environment
 if %ENVNAME%==base (set ENVPATH=%MAMBAPATH%) else (set ENVPATH=%MAMBAPATH%\envs\%ENVNAME%)
 call %MAMBAPATH%\Scripts\activate.bat %ENVPATH%
 
-rem Run the python script 
+:: Run the python script 
 python new_tracks.py -s %SPOTIFY_YAML% -g %GENRE% -d %DAYS% -a %ARTISTS_LIST%
 pause
 
-rem Deactivate the environment
+:: Deactivate the environment
 call mamba deactivate
