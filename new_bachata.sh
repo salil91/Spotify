e6eb09223c18621615bbd8deaf6e505f5f2cd789
Salil Bavdekar <salil.bavdekar@ufl.edu>
2024-07-30 22:00:47 -0400

    added sh file for linux

---
+++ new_bachata.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the parameters for the search
+DAYS=7
+ARTISTS_LIST="Bachata Artists (Manual).csv"
+GENRE=bachata  # only used to name the playlist, since artist list is given
+
+# Define the path to the yaml file with the spotify API parameters
+SPOTIFY_YAML=spotify.yaml
+
+# Set the working directory
+cd /home/salil.bavdekar/GitHub/Spotify/
+
+# Define the path to your conda installation
+export PATH=/blue/subhash/salil.bavdekar/.conda/envs/spotify/bin/:$PATH
+
+# Run the python script
+python new_tracks.py -s $SPOTIFY_YAML -g $GENRE -a "$ARTISTS_LIST" -d $DAYS
