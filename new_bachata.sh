324cfe8e0fcf24b6bb309bdd6e58522e414ecfa3
Salil Bavdekar <salil.bavdekar@ufl.edu>
2024-09-20 15:21:56 -0400

    changed sort order

--- new_bachata.sh
+++ new_bachata.sh
@@ -12,7 +12,7 @@ SPOTIFY_YAML=spotify.yaml
 # Set the working directory
 cd /home/$USER/GitHub/Spotify/
 
-# Define the path to your conda installation
+# Define the path to your python environment
 export PATH=/blue/subhash/salil.bavdekar/.conda/envs/spotify/bin/:$PATH
 
 # Run the python script
